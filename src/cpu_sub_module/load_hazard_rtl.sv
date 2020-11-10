1b6eed99c15fe7263e019c39051ac247095a50cc
“Jason-Du(NCKU) <53274895+Jason-Du@users.noreply.github.com>
2020-11-10 11:31:17 +0800

    1110 1131

--- src/cpu_sub_module/load_hazard_rtl.sv
+++ src/cpu_sub_module/load_hazard_rtl.sv
@@ -26,7 +26,8 @@ begin
 	//pc_stall=(id_exe_read_mem==1'b1 && (if_id_rs1_addr==id_exe_rd_addr || if_id_rs2_addr==id_exe_rd_addr))?1'b1:1'b0;
 	//instruction_stall=pc_stall;
 	pc_stall_stage1=(id_exe_read_mem==1'b1 && (if_id_rs1_addr==id_exe_rd_addr || if_id_rs2_addr==id_exe_rd_addr))?1'b1:1'b0;
-	pc_stall=bus_stall?1'b0:(pc_jump_confirm?1'b0:pc_stall_stage1);
+	pc_stall=pc_jump_confirm?1'b0:pc_stall_stage1;
+	//pc_stall=bus_stall?1'b0:(pc_jump_confirm?1'b0:pc_stall_stage1);
 	instruction_stall=pc_stall;
 end
 endmodule
